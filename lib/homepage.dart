cc83ded0caa2c6e4b57ceadce28decf806595cf5
bhartikumarigupta <shadhviraj97099@gmail.com>
2024-03-18 21:45:30 +0530

    deployed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -371,7 +371,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               "BABYCARE",
                               "A complete guide for your new born baby",
                               "3 mins",
-                              "assets/image/p1.png"),
+                              "assets/image/p1.jpg"),
                           SizedBox(
                             width: 16,
                           ),
